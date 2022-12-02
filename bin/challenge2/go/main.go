0d41ff4be3f26f12e7f9ffdfdf3770b370081b08
Tullis, Sean <seantullis0@gmail.com>
2022-12-01 22:15:44 -0700

    admin: Remove Go binary for Challenge 1

--- bin/challenge2/go/main.go
+++ bin/challenge2/go/main.go
@@ -1,7 +1,22 @@
 package main
 
 import (
+    "fmt"
+    "log"
+    "os"
+    "path/filepath"
+
+    "aoc2022/utility"
 )
 
 func main() {
+    p, err := filepath.Abs(os.Args[1])
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+
+    text, err := utility.ReadFile(p)
+    if err != nil {
+        log.Fatal(err.Error())
+    }
 }
